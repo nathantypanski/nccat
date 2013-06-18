f6a4ee92a866930267e41a7636c63e92609909e9
Nathan Typanski <ntypanski@gmail.com>
2013-06-18 17:48:51 -0400

    post handler

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 var http = require("http");
-var os = require("os");
-var path = require("path");
 var util = require("util");
-var fs = require("fs");
 var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname, response);
+        request.setEncoding("utf8");
+
+        console.log("adding data listener ...");
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '"+
+                postDataChunk + "'.");
+        });
+
+        console.log("adding request listener ...");
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
-    /*
-    fs.watch('./', function (event, filename) {
-      console.log('event is: ' + event);
-      if (filename != "*.swp") {
-        console.log('filename provided: ' + filename);
-      } else {
-        console.log('filename not provided');
-      }
-    });
-    */
 
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
-
-    console.log(os.hostname() + "@" + os.release());
-//    util.puts("uname -r");
 }
 
 exports.start = start;
